2bfe6d69ecc9d616b366b35f72aec9e41f6206c6
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-07-22 16:16:03 +0400

    ex1 проверка возращаемых бинов типа Singleton Стериотипные аннотации

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,15 +2,16 @@ package main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
+import services.UserService;
 
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        var c1 = context.getBean("commentService", CommentService.class);
-        var c2 = context.getBean("commentService", CommentService.class);
+        var c1 = context.getBean(CommentService.class);
+        var c2 = context.getBean(UserService.class);
 
-        boolean b1 = c1 == c2;
+        boolean b1 = c1.getRepository() == c2.getRepository();
         System.out.println(b1);
 
     }
--- src/main/java/main/ProjectConfig.java
+++ src/main/java/main/ProjectConfig.java
@@ -1,10 +1,13 @@
 package main;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import services.CommentService;
 
 @Configuration
+@ComponentScan(basePackages =  {"repositories","services"})
 public class ProjectConfig {
 
     @Bean
