f7f263afc2cfac4f87170b480aee5148a0957f6b
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-07-22 16:40:46 +0400

    ex2 ленивое и немедленное создание бинов @Lazy

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,10 +1,12 @@
 package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import repositories.CommentRepository;
 
 @Service
+@Lazy
 public class CommentService {
 
     public CommentService() {
